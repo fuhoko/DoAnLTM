1996fab3bdf5dfb504af01eb2d261e86d1bba10e
unknown <laki4uall@gmail.com>
2020-12-27 12:32:46 +0700

    config heroku

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-app.listen(3001);
+app.listen(process.env.PORT || 3000);
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
